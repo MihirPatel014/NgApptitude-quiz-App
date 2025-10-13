9f024c9dd5b78771a4c1b1668dd33a93d4502b98
Mihir Patel <patelmihir.6142@gmail.com>
2025-10-13 14:39:35 +0530

    modified tailwind.config.js files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
-    // "./node_modules/flowbite/**/*.js",// Tailwind will scan these files
     "./node_modules/tailwind-datepicker-react/dist/**/*.js", 
-    "./**/*.html",
 
   ],
   theme: {
